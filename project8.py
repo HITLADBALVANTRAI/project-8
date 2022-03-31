12d29a3c9418dbeaf944541cc2ada015b202733f
HITLADBALVANTRAI <101315912+HITLADBALVANTRAI@users.noreply.github.com>
2022-03-31 21:10:35 +0530

    Add files via upload

---
+++ project8.py
@@ -0,0 +1,15 @@
+n1 = int(input("Enter the first number"))
+n2 = int(input("Enter the second number"))
+
+def addition(n1,n2):
+    print(n1+n2)
+def subtraction(n1,n2):
+    print(n1-n2)
+def multiplaction(n1,n2):
+    print(n1*n2)
+def division(n1,n2):
+    print(n1/n2)    
+addition(n1,n2)
+subtraction(n1,n2)
+multiplaction(n1,n2)
+division(n1,n2)
